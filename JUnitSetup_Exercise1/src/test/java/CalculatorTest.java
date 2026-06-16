cf54ab3795086e0b93c778dcaa93c00690876204
raikarshubham <raikarshubhamvaikunt@gmail.com>
2026-06-17 00:21:30 +0530

    junits

---
+++ JUnitSetup_Exercise1/src/test/java/CalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        assertEquals(5, 2 + 3);
+    }
+}
